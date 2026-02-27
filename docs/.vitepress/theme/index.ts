8b117958d9c90cd198c5ba895fc88789e6a20a48
yuiyide <yuiyide@gmail.com>
2026-02-27 15:39:45 +0800

    feat:更新导入

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -5,7 +5,7 @@ import type { App } from 'vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 // 2. 引入你的组件库
-import everybody from 'everybody-ui/dist/everybody'
+import everybody from 'everybody-ui'
 import 'everybody-ui/dist/everybody.css'
 import './custom.css'
 export default {
